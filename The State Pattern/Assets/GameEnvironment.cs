b6111bdce29b9b239909863ecb439c076d9e8351
Pedro Braga <phbm.braga@gmail.com>
2021-02-19 09:48:30 -0300

    Creating and using a state class

---
+++ The State Pattern/Assets/GameEnvironment.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment 
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public GameEnvironment Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+
+    public void AddChechpoint(GameObject c)
+    {
+        Instance.checkpoints.Add(c);
+    }
+
+    public void RemoveCheckpoint(GameObject c)
+    {
+        Instance.checkpoints.Remove(c);
+    }
+   
+}
---
+++ The State Pattern/Assets/Idle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Idle : State
+{
+    public Idle (GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        // Clean all the triggers in the queue
+        anim.ResetTrigger("isIdle");
+        base.Exit()
+    }
+}
